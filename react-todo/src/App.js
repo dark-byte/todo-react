b1f14d37f7430c2a9a7c0a30843496a2cbce17b2
Adrish Mitra <46316785+dark-byte@users.noreply.github.com>
2021-11-30 00:50:44 +0530

    added local storage feature

--- react-todo/src/App.js
+++ react-todo/src/App.js
@@ -1,12 +1,33 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Form from './Components/Form';
 import TodoList from './Components/TodoList';
 
 function App() {
-  
+
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  
+  useEffect(() =>{
+    getLocalTodos();
+  }, []);
+  
+  useEffect(()=>{
+    saveLocalTodos();
+  }, [todos]);
+
+  const saveLocalTodos = () =>{
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const getLocalTodos = () =>{
+    if (localStorage.getItem("todos" === null)){
+      localStorage.setItem("todos". JSON.stringify([]));
+    } else {
+      let localTodos = JSON.parse(localStorage.getItem("todos"));
+      setTodos(localTodos);
+    }
+  };
 
   return (
     <div className="App">
